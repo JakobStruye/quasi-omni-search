797187664662cc389e5dd8726fa784c20e5bfa90
Jakob Struye <jakob.struye@uantwerpen.be>
2024-01-23 16:46:51 +0100

    Planar version

--- arraysynthesis_ex.m
+++ arraysynthesis_ex.m
@@ -1,21 +1,45 @@
 %% Array Synthesis
 %  Copyright 2017  The MathWorks, Inc.
 %% Optimize pattern using only weights 
-N = 8;                                            % 8 elements in a linear array
-azimuth = -90:90;                                 % Define azimuth field of view
-weights_d = hamming(N);                           % Create weights for desired pattern 
-weights_d = weights_d/norm(weights_d);            % Normalize weights for pattern
-stvmat = steervec((0:N-1)/2,azimuth);             % Generate a pattern for this example
-Beam_d = weights_d'*stvmat;                       % Apply weights to desired pattern
-%% Set up optimization 
-objfun = @(w)norm(w'*stvmat-Beam_d);     % Define objective function used in fmincon
-                                              % Goal is to minimize the norms between
-                                              % the desired pattern and
-                                              % resulting pattern
-weights_i = ones(N,1);                        % Initial setting for array amplitudes
+format long g
+N_x = 64;                                            % 8 elements in a linear array
+N_y = 64;
+azimuth = -90:1:90;                                 % Define azimuth field of view
+elevation = -90:1:90;
+%elevation = 0;
+azrep = repmat(azimuth, 1, size(elevation,2));
+elrep = repelem(elevation, 1, size(azimuth,2));
+angs = [azrep; elrep];
+%disp(angs);
+azrand = 180 * rand(1,1000) - 90;
+elrand = 180 * rand(1,1000) - 90;
+angsrand = [azrand;elrand];
+angstrain = angsrand;
+angstest = angs;
+%weights_d = hamming(N);                           % Create weights for desired pattern 
+%weights_d = ones(N,1);3j6ilBYnhcjl
+%weights_d = weights_d/norm(weights_d);            % Normalize weights for pattern
+locs_x = (0:N_x-1)/4 - ((N_x-1)/8);
+locs_y = (N_y-1:-1:0)/4 - ((N_y-1)/8);
+locs_x = imresize(locs_x, [1 N_x*N_y], 'nearest');
+locs_y = repmat(locs_y,1, N_x);
+locs = [locs_x; locs_y];
+stvmat = steervec(locs,angstrain);             % Generate a pattern for this example
+%Beam_d = weights_d'*stvmat;                       % Apply weights to desired pattern
+%disp(size(Beam_d));
+Beam_d = ones(1,size(angstrain,2));
+%% Set up optimization
+objfun = @(ph)max(abs(exp(1i*ph)'*stvmat))- min(abs(exp(1i*ph)'*stvmat)); 
+%objfun = @(ph)(1.0/max(abs(exp(1i*ph)'*stvmat))); 
+
+weights_i = 1*ones(N_x*N_y,1);                        % Initial setting for array amplitudes
                                               % Serves as starting point to
                                               % optimization
-weights_o = fmincon(objfun,weights_i,[],[],[],[],zeros(N,1),ones(N,1));
+options = optimoptions(@fmincon,'Algorithm','active-set','Display','off');
+
+tic;
+
+weights_o = fmincon(objfun,weights_i,[],[],[],[],-1*pi*ones(N_x*N_y,1),1*pi*ones(N_x*N_y,1), [], options);
                                              % fmincon takes in the objfun,
                                              % the initial weights, and 
                                              % upper and lower bounds of the weights   
@@ -25,32 +49,22 @@ weights_o = fmincon(objfun,weights_i,[],[],[],[],zeros(N,1),ones(N,1));
                                              % which can be used to create
                                              % a beam that matches our
                                              % desired pattern
- %%                                           
+                                             
+after = toc;
+%%                 
+stvmattest = steervec(locs,angstest);
+disp(weights_o);
+disp(after);
+disp(mean(abs(exp(1i*weights_o)'*stvmattest)))
+return;
+
 figure;
-plot(azimuth,mag2db([abs(Beam_d);abs(weights_o'*stvmat)]'))
+t = tiledlayout(2,1);
+nexttile;
+plot(1:size(angstrain,2),mag2db([abs(Beam_d);abs(exp(1i*weights_o)'*stvmat)]'))
+%ylim([-50, 30]);
 legend('desired','synthesized')
-ylim([-100, 50]);
-%% Optimize Both Weights and Element Position
-% Build Desired Pattern
-N = 8;                                        % 8 elements in array
-azimuth = -90:90;
-weights_d = hamming(N).*steervec((0:N-1)/2,10);
-stvmat = steervec((0:N-1)/2,azimuth);
-Beam_d = abs(weights_d'*stvmat);
-% Set up optimization
-% break complex weights to real and imaginary
-w_i_re = ones(N,1);                          % initial real values
-w_i_im = zeros(N,1);                         % initial imaginary values 
-x_i = (0:N-1)'/2;                            % initial locations - uniform
-objfun = @(x)norm(abs((x(1:N)+1i*x(N+1:2*N))'*steervec(x(2*N+1:end).',azimuth))-Beam_d);
-x_ini = [w_i_re;w_i_im;x_i];
-[x_o, fval, ef, output] = ...
-    fmincon(objfun,x_ini,[],[],[],[],[-ones(2*N,1);(0:N-1)'/2-0.2],...
-     [ones(2*N,1);(0:N-1)'/2+0.2],...
-    [], optimoptions(@fmincon,'Display','iter'));
-%%
-figure;
-plot(azimuth,mag2db([Beam_d;abs((x_o(1:N)+1i*x_o(N+1:2*N))'*steervec(x_o(2*N+1:end).',azimuth))]'))
-legend('desired','synthesized')
-%%
-scatter(x_o(2*N+1:end), zeros(1,8))
+nexttile;
+plot(1:size(angstest,2),mag2db([abs(exp(1i*weights_o)'*stvmattest)]'))
+%ylim([-50, 30]);
+return;
